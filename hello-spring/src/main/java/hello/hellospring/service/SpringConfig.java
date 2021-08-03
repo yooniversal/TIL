31b782f27f5c86c95d900d3e49ef83936b242ec9
yooniversal <iteratively@naver.com>
2021-08-03 18:12:09 +0900

    AOP 적용

--- hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring.service;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
